aa2377845b577c707e38ead3851301a7294664ab
Alejandro Martinez Torres <alejandromartinezt@gmail.com>
2022-05-02 21:22:47 -0500

    Borrando todo lo que no se necesita o ya esta en otro lado

--- parche/normal.py
+++
@@ -1,9 +0,0 @@
-file = open("admin.jsonl", "r")
-out = open("output.jsonl", "w", encoding='utf-8')
-dict = file.readlines()
-for string in dict:
-    string = string.encode('utf-8')
-    print(string)
-    tr = string.decode('unicode-escape')
-    print(tr)
-    out.write(tr)
