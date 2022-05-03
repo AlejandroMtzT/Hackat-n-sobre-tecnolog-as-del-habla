e895bf214b00d0871d60967ad64d133d06b4f2b2
DanielO9803 <73365139+DanielO9803@users.noreply.github.com>
2022-05-03 12:31:21 -0500

    Formulario inicial

--- amoxcalli/src/components/Formulario.js
+++ amoxcalli/src/components/Formulario.js
@@ -2,15 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../App.css';
 
-
 export default class Formulario extends React.Component {
   render() {
     console.log('adios');
     return (
-      <div className="App">
-      <p>
-      FUNCIONA
-      </p>
+      <div className="Formulario">
+        <header className="Formulario-header">
+
+          <form action = "" method="post" />
+            <center><label for="Título">Título</label></center>
+            <center><textarea cols="30" rows="5" type="text" id="Título" name="Título" placeholder="Introduce el título de tu contribución" /></center>
+            <br></br>
+            <br />
+            <center><label for="Fuente">Fuente</label></center>
+            <center><textarea cols="30" rows="5" type="Fuente" id="Fuente" name="Fuente" placeholder="Introduce la fuente" /></center>
+            <br />
+            <center><label for="Subtítulo">Subtítulo</label></center>
+            <center><textarea cols="30" rows="5" type="Subtítulo" id="Subtítulo" name="Subtítuloo" placeholder="Introduce el Subtítulo" /></center>
+            <br />
+            <center><label for = 'Texto'>Texto</label></center>
+            <center><textarea cols="50" rows="10" id="Texto" placeholder="Introduce el texto de tu contribución" name="Texto"></textarea> </center>
+            <br />
+            <center><input type="submit" /></center>
+
+        </header>
       </div>
 
     );
