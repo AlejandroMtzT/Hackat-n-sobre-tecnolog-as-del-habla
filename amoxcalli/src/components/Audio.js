d37e40fcea9bbd4761c7da999fd97863224211a0
Roberto Ledesma <ledesma@Robertos-MacBook-Pro.local>
2022-05-03 17:36:07 -0500

    se descargan audios

--- amoxcalli/src/components/Audio.js
+++ amoxcalli/src/components/Audio.js
@@ -1,97 +1,31 @@
-import React, { Component } from "react";
-import AudioAnalyser from "react-audio-analyser";
+import {useState} from 'react';
+import { saveAs } from 'file-saver';
+import '../App.css';
 
-export default class Audio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: ""
-    };
+function Audio (){
+
+  const [soundFile, setSoundFile] = useState({});
+  var FileSaver = require('file-saver');
+
+  const handleChangeSound = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    setSoundFile(file);
   }
 
-  controlAudio(status) {
-    this.setState({
-      status
-    });
+  const guardarAudio = async () => {
+    FileSaver.saveAs(soundFile);
   }
 
-  changeScheme(e) {
-    this.setState({
-      audioType: e.target.value
-    });
-  }
+  return(
+    <div className="App">
+      <p></p>
+      <input type="file" id="sound" accept="sound/*" onChange={handleChangeSound}></input>
+      <p></p>
+      <button onClick={guardarAudio}>GUARDAR</button>
 
-  componentDidMount() {
-    this.setState({
-      audioType: "audio/wav"
-    });
-  }
-
-  render() {
-    const { status, audioSrc, audioType } = this.state;
-    const audioProps = {
-      audioType,
-      // audioOptions: {sampleRate: 30000}, // 设置输出音频采样率
-      status,
-      audioSrc,
-      timeslice: 1000, // timeslice（https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/start#Parameters）
-      startCallback: e => {
-        console.log("succ start", e);
-      },
-      pauseCallback: e => {
-        console.log("succ pause", e);
-      },
-      stopCallback: e => {
-        this.setState({
-          audioSrc: window.URL.createObjectURL(e)
-        });
-        console.log("succ stop", e);
-        console.log(e.stream());
-        console.log(URL.createObjectURL(e));
-      },
-      onRecordCallback: e => {
-        console.log("recording", e);
-      },
-      errorCallback: err => {
-        console.log("error", err);
-      }
-    };
-    return (
-      <div>
-        <AudioAnalyser {...audioProps}>
-          <div className="btn-box">
-            <button
-              className="btn"
-              onClick={() => this.controlAudio("recording")}
-            >
-              Start
-            </button>
-            <button className="btn" onClick={() => this.controlAudio("paused")}>
-              Pause
-            </button>
-            <button
-              className="btn"
-              onClick={() => this.controlAudio("inactive")}
-            >
-              Stop
-            </button>
-            <button className="btn" onClick={() => console.log(audioType)}>
-              Log
-            </button>
-          </div>
-        </AudioAnalyser>
-        <p>choose output type</p>
-        <select
-          name=""
-          id=""
-          onChange={e => this.changeScheme(e)}
-          value={audioType}
-        >
-          <option value="audio/webm">audio/webm（default）</option>
-          <option value="audio/wav">audio/wav</option>
-          <option value="audio/mp3">audio/mp3</option>
-        </select>
-      </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Audio;
