de8d171438745dbddcd2d3e6f60ceca7c6d1ee67
Kmabay <uncuatedeporahi@gmail.com>
2022-05-03 05:21:19 -0500

    Amoxcalli 1.3

---
+++ Amoxcalli_1.3/amoxcalli/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import {Inicio} from './Inicio';
+import {Formulario} from './Formulario';
+import {BrowserRouter, Route, Routes, NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      
+        <h2 className="d-flex justify-content-center m-3">
+          Amoxcalli
+        </h2>
+          <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
+            <ul className='navbar-nav'>
+              <li className='nav-item- m-1'>
+                <NavLink className="btn btn-light btn-outline-primary" to="/inicio">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className='nav-item- m-1'>
+                <NavLink className="btn btn-light btn-outline-primary" to="/formulario">
+                  Formulario
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/inicio"  element={<Inicio/>}/>
+            <Route path='/formulario' element={<Formulario/>}/>
+          </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Amoxcalli_1.3/amoxcalli/src/Formulario.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Formulario extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Esta es la página de formulario</h3>
+            </div>
+        )
+    }
+}
